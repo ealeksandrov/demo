530be6a0c5e01a9b95c125215a6b110693a60d06
Evgeny Aleksandrov <evgeny@aleksandrov.ws>
2018-07-10 10:50:07 +0300

    Add person stats

---
+++ Source/Views/PersonStatsView.swift
@@ -0,0 +1,17 @@
+//
+//  PersonStatsView.swift
+//  Demo
+//
+//  Created by Evgeny Aleksandrov on 10/07/2018.
+//  Copyright © 2018 demo. All rights reserved.
+//
+
+import UIKit
+
+class PersonStatsView: UIView {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var calLabel: UILabel!
+
+}
